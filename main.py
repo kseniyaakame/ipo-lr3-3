f5a857015068871d5a55f3504c3de50b23e96781
Kseniya <147338017+kseniyaakame@users.noreply.github.com>
2024-10-01 10:11:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 d = int(input('Введите день '))
 m = int(input('Введите месяц '))
 # Проверяем, к какому времени года относится введенная дата
-if (m == 9 and d >= 1) or (m > 9 and m < 12) or (m == 11 and d <= 30):
+if (m == 9 and  d < 31 and d >= 1 ) or (m == 10  and d <= 31 and d >= 1 ) or (m == 11 and d <= 30 and d >= 1):
     s = 'Осень'
-elif (m == 3 and d >= 1) or (m > 3 and m < 6) or (m == 5 and d <= 31):
+elif (m == 3 and d <= 31 and d >= 1) or (m == 4 and d <= 30 and d >= 1) or (m == 5 and d <= 31 and d >= 1):
     s = 'Весна'
-elif (m == 12 and d >= 1) or (m > 0 and m < 3) or (m == 2 and d <= 29):
+elif (m == 12 and d >= 1 and d <= 31) or (m == 1 and d <= 31 and d >= 1) or (m == 2 and d <= 29 and d >= 1):
     s = 'Зима'
 else:
     s = 'Лето'
